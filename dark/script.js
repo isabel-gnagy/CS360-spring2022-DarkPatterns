838a363de4053f6a62dcb56fbf7b3cbe87e1181d
Isabel Gnagy <76077843+isabel-gnagy@users.noreply.github.com>
2022-04-16 16:56:27 -0400

    Update script.js

--- dark/script.js
+++ dark/script.js
@@ -1,13 +1,16 @@
 function cartPriceUpdate(){
     var fee2 = document.getElementById("cart-fee2").textContent;
     var num = document.getElementById("cart-quantity-input").value;
+    var num1 = document.getElementById("cart-quantity-input-1").value;
     var item = document.getElementById("item-price").textContent;
+    var item1 = document.getElementById("item-price-1").textContent;
     var tot = document.getElementById("cart-total");
     var agr = document.getElementById("agree-price");
 
     fees = parseInt(fee2);
     var subtotal = item*num;
-    finaltotal = parseInt(subtotal)+parseInt(fees);
+    var subtotal1 = item1*num1;
+    finaltotal = parseInt(subtotal)+parseInt(subtotal1)+parseInt(fees);
 
     tot.innerHTML = finaltotal+".00";
     agr.innerHTML = finaltotal+".00";
